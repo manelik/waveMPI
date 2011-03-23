a54e32b0da31d3d8f85ada62fd10cc29615f5535
Jose Manuel Torres <josemanuel.tch@gmail.com>
2011-03-23 01:11:00 -0600

    improved runscript, now self explanatory

--- runscript.sh
+++ runscript.sh
@@ -4,5 +4,12 @@
 #
 # runscript.sh numproc binary input
 
+if [ ${#@} -lt 3 ]
+    then
+    echo 'Bad usage'
+    echo $0' numproc binary input'
+    exit 0
+fi
+
 echo 'Script '$0' running'
 mpirun -n $1 $2 < $3
